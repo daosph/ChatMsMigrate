5235e0f3437691f3df6cb8d80b62a588d56ceedf
david <davidhernandez9801@gmail.com>
2024-08-20 04:16:57 -0500

    Initial commit

---
+++ src/test/java/chatsocket/chatmigrate/ChatMigrateApplicationTests.java
@@ -0,0 +1,13 @@
+package chatsocket.chatmigrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMigrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
